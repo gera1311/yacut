e2dd6c0d0008b9515a168cae87f4407e381d6783
gera1311 <gera.python@yandex.ru>
2025-03-11 18:12:56 +0300

    Constants fixed errors

--- yacut/constants.py
+++ yacut/constants.py
@@ -4,9 +4,7 @@ LENGTH_RANDOM_SHORT = 6
 MAX_LENGTH_SHORT = 16
 MAX_LENGTH_ORIGINAL = 2000
 MAX_ATTEMPTS = 100
-ALLOWED_CHARS = ''.join([
-    string.ascii_uppercase,
-    string.ascii_lowercase,
-    string.digits,
-])
+ALLOWED_CHARS = f'{string.ascii_uppercase}' \
+                f'{string.ascii_lowercase}' \
+                f'{string.digits}'
 REGEX_SHORT = rf'^[{ALLOWED_CHARS}]*$'
